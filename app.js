1b523b63620f04af79cd0bf6e5633d8302a6437e
Klimenko Sveta <rosedogknight@yandex.ru>
2022-11-19 16:52:09 +0300

    13 project quotes fixed

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-import express from "express";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import { constants } from "http2";
-import { user } from "./routes/users.js";
-import { card } from "./routes/cards.js";
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import { constants } from 'http2';
+import { user } from './routes/users.js';
+import { card } from './routes/cards.js';
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
-    _id: "6373cfe5b06f20de4f4dac7f",
+    _id: '6373cfe5b06f20de4f4dac7f',
   };
 
   next();
 });
 
-app.use("/", user);
-app.use("/", card);
+app.use('/', user);
+app.use('/', card);
 
-app.all("/*", (req, res) => {
+app.all('/*', (req, res) => {
   res
     .status(constants.HTTP_STATUS_NOT_FOUND)
-    .send({ message: "Страница не найдена" });
+    .send({ message: 'Страница не найдена' });
 });
 
 app.listen(PORT, () => {
