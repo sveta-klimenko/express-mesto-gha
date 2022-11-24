4a9bca2758cdfe0cb8442e565822d96e615bd58b
Klimenko Sveta <rosedogknight@yandex.ru>
2022-11-24 14:26:08 +0300

    13 project part 1

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -6,16 +7,42 @@ const userSchema = new mongoose.Schema({
     required: true,
     minLength: 2,
     maxLength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     required: true,
     minLength: 2,
     maxLength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
     required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator(v) {
+        const linkRegex = /^https?:\/\/(www.)?[\w\-._~:\/?#[\]@!$&'()*+,;=]*#?$/;
+        return linkRegex.test(v);
+      },
+      message: 'Неверный формат ссылки на аватар',
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: 'Почта указана неверно',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
   },
 });
 
